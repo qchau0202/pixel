077f7b251acb5558178fb3d4528c8a7db5eeacdf
iamchouty <quocchau4729@gmail.com>
2023-11-03 16:15:33 +0700

    Updated, bug fixes

--- src/components/CustomClock.jsx
+++ src/components/CustomClock.jsx
@@ -1,91 +1,112 @@
-import React, { useState, useEffect } from "react";
-import {
-  CircularProgressbarWithChildren,
-} from "react-circular-progressbar";
+import React, { useState, useEffect, useRef } from "react";
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
 import ProgressProvider from "./ProgressProvider";
 
 const CustomClock = () => {
+  const [isPaused, setIsPaused] = useState(true);
+  const [progress, setProgress] = useState(100);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const playButtonClickSound = () => {
+    const audio = document.getElementById("playSound");
+    audio.play();
+  };
+
+  const playAlarmSound = () => {
+    const audio = document.getElementById("alarmSound");
+    audio.play();
+  };
+
+  const audioRef = useRef();
+
+  const resetAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  };
+
+  const handlePlayButtonClick = () => {
+    if (minutes > 0 || seconds > 0) {
+      setIsPaused(false);
+      resetAudio();
+      playButtonClickSound();
+    }
+  };
+
   const resetBtn = () => {
     setMinutes(0);
     setSeconds(0);
     setProgress(100);
+    resetAudio();
   };
 
   const add10 = () => {
     setMinutes(minutes + 10);
+    resetAudio();
   };
+
   const sub10 = () => {
-    if (minutes < 0) {
-      alert("Invalid time");
-      setMinutes(0);
-      setSeconds(0);
-      setProgress(100);
-    } else {
+    if (minutes >= 10) {
       setMinutes(minutes - 10);
+      resetAudio();
     }
   };
 
   const add5 = () => {
     setMinutes(minutes + 5);
+    resetAudio();
   };
+
   const sub5 = () => {
-    if (minutes < 0) {
-      alert("Invalid time");
-      setMinutes(0);
-      setSeconds(0);
-    } else {
+    if (minutes >= 5) {
       setMinutes(minutes - 5);
+      resetAudio();
     }
   };
-
-  const initialSeconds = 0;
-  const initialMinute = 0;
-  const [isPaused, setIsPaused] = useState(true);
-  const [progress, setProgress] = useState(100);
-
-  const [minutes, setMinutes] = useState(initialMinute);
-  const [seconds, setSeconds] = useState(initialSeconds);
 
   useEffect(() => {
-  const myInterval = setInterval(() => {
-    if (isPaused) {
-      return;
-    }
-    if (minutes === 0 && seconds === 0) {
-      clearInterval(myInterval);
-      return;
-    }
-    if (minutes < 0) {
-      clearInterval(myInterval);
-      setIsPaused(true); 
-      setMinutes(0);
-      setSeconds(0);
-      setProgress(100); 
-      return;
+    let myInterval;
+
+    if (!isPaused) {
+      myInterval = setInterval(() => {
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(myInterval);
+          setIsPaused(true);
+          setMinutes(0);
+          setSeconds(0);
+          setProgress(100);
+          playAlarmSound();
+        } else if (seconds === 0) {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        } else {
+          setSeconds(seconds - 1);
+        }
+
+        setProgress(
+          Math.round(((minutes * 60 + seconds) / (initialMinute * 60)) * 100)
+        );
+      }, 1000);
     }
 
-    if (seconds > 0) {
-      setSeconds(seconds - 1);
-      setProgress(
-        Math.round(
-          ((minutes * 60 + seconds - 1) / (initialMinute * 60)) * 100
-        )
-      );
-    } else {
-      if (minutes > 0) {
-        setMinutes(minutes - 1);
-        setSeconds(59);
-      }
-    }
-  }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [isPaused, minutes, seconds]);
 
-  return () => {
-    clearInterval(myInterval);
-  };
-}, [isPaused, minutes, seconds]);
+  const initialMinute = 0;
 
   return (
     <div className="clock-holder center custom-holder">
+      <audio id="playSound" ref={audioRef}>
+        <source src="/audio/mixkit-arcade-game-jump-coin-216.mp3" type="audio/mpeg" />
+      </audio>
+      <audio id="alarmSound" ref={audioRef}>
+        <source src="/audio/mixkit-alert-quick-chime-766.mp3" type="audio/mpeg" />
+      </audio>
+
       <div className="clock">
         <ProgressProvider valueStart={100} valueEnd={progress}>
           {() => (
@@ -103,40 +124,37 @@ const CustomClock = () => {
 
       <div className="play-btn center">
         {isPaused ? (
-          <button onClick={() => setIsPaused(false)}>
-            <img src="play-button-svgrepo-com.svg" />
+          <button onClick={handlePlayButtonClick}>
+            <img src="play-button-svgrepo-com.svg" alt="Play" />
           </button>
         ) : (
           <button onClick={() => setIsPaused(true)}>
-            <img src="pause-svgrepo-com.svg" alt="" />
+            <img src="pause-svgrepo-com.svg" alt="Pause" />
           </button>
         )}
-        <button
-          onClick={() => resetBtn()}
-          disabled={!isPaused}
-          className="reset"
-        >
-          <img src="reset-svgrepo-com.svg" alt="" />
+        <button onClick={resetBtn} disabled={!isPaused} className="reset">
+          <img src="reset-svgrepo-com.svg" alt="Reset" />
         </button>
       </div>
+
       <div className="button-holder center">
         <div className="tenMin">
-          <button className="up" onClick={() => add10()} disabled={!isPaused}>
-            <img src="round-arrow-up-svgrepo-com.svg" alt="" />
+          <button className="up" onClick={add10} disabled={!isPaused}>
+            <img src="round-arrow-up-svgrepo-com.svg" alt="+10" />
             <p>+10</p>
           </button>
-          <button className="down" onClick={() => sub10()} disabled={!isPaused}>
-            <img src="round-arrow-down-svgrepo-com.svg" alt="" />
+          <button className="down" onClick={sub10} disabled={!isPaused}>
+            <img src="round-arrow-down-svgrepo-com.svg" alt="-10" />
             <p>-10</p>
           </button>
         </div>
         <div className="fiveMin">
-          <button className="up" onClick={() => add5()} disabled={!isPaused}>
-            <img src="round-arrow-up-svgrepo-com.svg" alt="" />
+          <button className="up" onClick={add5} disabled={!isPaused}>
+            <img src="round-arrow-up-svgrepo-com.svg" alt="+5" />
             <p>+5</p>
           </button>
-          <button className="down" onClick={() => sub5()} disabled={!isPaused}>
-            <img src="round-arrow-down-svgrepo-com.svg" alt="" />
+          <button className="down" onClick={sub5} disabled={!isPaused}>
+            <img src="round-arrow-down-svgrepo-com.svg" alt="-5" />
             <p>-5</p>
           </button>
         </div>
--- src/components/PomodoroClock.jsx
+++ src/components/PomodoroClock.jsx
@@ -1,90 +1,118 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   CircularProgressbarWithChildren,
 } from "react-circular-progressbar";
 import ProgressProvider from "./ProgressProvider";
 
 const PomodoroClock = () => {
+  const [isPaused, setIsPaused] = useState(true);
+  const [progress, setProgress] = useState(100);
+  const [minutes, setMinutes] = useState(25);
+  const [seconds, setSeconds] = useState(0);
+
+  const playButtonClickSound = () => {
+    const audio = document.getElementById("playSound");
+    audio.play();
+  };
+
+  const playAlarmSound = () => {
+    const audio = document.getElementById("alarmSound");
+    audio.play();
+  };
+
+  const resetAudio = () => {
+    const audio = document.getElementById("playSound");
+    audio.currentTime = 0;
+  };
+
   const resetBtn = () => {
     setMinutes(25);
     setSeconds(0);
     setProgress(100);
-  }
-  const initialMinute = 25;
-  const initialSeconds = 0;
-  const [isPaused, setIsPaused] = useState(true);
-  const [progress, setProgress] = useState(100);
-
-  const [minutes, setMinutes] = useState(initialMinute);
-  const [seconds, setSeconds] = useState(initialSeconds);
+    resetAudio();
+    setIsPaused(true); // Reset the timer to paused state
+  };
 
   useEffect(() => {
-    const myInterval = setInterval(() => {
-      if (isPaused) {
-        return;
-      }
-      if (seconds > 0) {
-        setSeconds(seconds - 1);
-        setProgress(
-          Math.round(
-            ((minutes * 60 + seconds - 1) / (initialMinute * 60)) * 100
-          )
-        );
-      }
-      if (seconds === 0) {
-        if (minutes === 0) {
-          // clearInterval(myInterval);
-          setMinutes(0)
-        } else {
-          setMinutes(minutes - 1);
-          setSeconds(59);
+    let myInterval;
+
+    if (!isPaused) {
+      myInterval = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds(seconds - 1);
+          setProgress(
+            Math.round(((minutes * 60 + seconds - 1) / (25 * 60)) * 100)
+          );
         }
-      }
-    }, 1000);
+
+        if (seconds === 0) {
+          if (minutes === 0) {
+            clearInterval(myInterval);
+            playAlarmSound(); // Play the alarm sound
+            setIsPaused(true); // Pause the timer
+          } else {
+            setMinutes(minutes - 1);
+            setSeconds(59);
+          }
+        }
+      }, 1000);
+    }
+
     return () => {
       clearInterval(myInterval);
     };
-  });
+  }, [isPaused, minutes, seconds]);
+
+  const audioRef = useRef();
 
   return (
     <div>
-      {minutes === 0 && seconds === 0 ? null : (
-        <>
-          <div className="center">
-            <div className="clock-holder center">
-              <div className="clock">
-                <ProgressProvider valueStart={100} valueEnd={progress}>
-                  {() => (
-                    <CircularProgressbarWithChildren value={progress}>
-                      <div className="time">
-                        <p>
-                          {" "}
-                          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-                        </p>
-                      </div>
-                    </CircularProgressbarWithChildren>
-                  )}
-                </ProgressProvider>
-              </div>
+      <div className="center">
+        <div className="clock-holder center">
+          <audio id="playSound" ref={audioRef}>
+            <source src="/audio/mixkit-arcade-game-jump-coin-216.mp3" type="audio/mpeg" />
+          </audio>
+          <audio id="alarmSound">
+            <source src="/audio/mixkit-alert-quick-chime-766.mp3" type="audio/mpeg" />
+          </audio>
 
-              <div className="play-btn center">
-                {isPaused ? (
-                  <button onClick={() => setIsPaused(false)}>
-                    <img src="play-button-svgrepo-com.svg" />
-                  </button>
-                ) : (
-                  <button onClick={() => setIsPaused(true)} >
-                    <img src="pause-svgrepo-com.svg" alt="" />
-                  </button>
-                )}
-                <button onClick={() => resetBtn()} disabled={!isPaused} className="reset">
-                  <img src="reset-svgrepo-com.svg" alt="" />
-                </button>
-              </div>
-            </div>
+          <div className="clock">
+            <ProgressProvider valueStart={100} valueEnd={progress}>
+              {() => (
+                <CircularProgressbarWithChildren value={progress}>
+                  <div className="time">
+                    <p>
+                      {" "}
+                      {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                    </p>
+                  </div>
+                </CircularProgressbarWithChildren>
+              )}
+            </ProgressProvider>
           </div>
-        </>
-      )}
+
+          <div className="play-btn center">
+            {isPaused ? (
+              <button
+                onClick={() => {
+                  setIsPaused(false);
+                  resetAudio();
+                  playButtonClickSound();
+                }}
+              >
+                <img src="play-button-svgrepo-com.svg" alt="Play" />
+              </button>
+            ) : (
+              <button onClick={() => setIsPaused(true)}>
+                <img src="pause-svgrepo-com.svg" alt="Pause" />
+              </button>
+            )}
+            <button onClick={resetBtn} className="reset">
+              <img src="reset-svgrepo-com.svg" alt="Reset" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
